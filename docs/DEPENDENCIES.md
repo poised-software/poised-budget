6bf17bb282f669bad8fe41f1a3d8726b2b8b139f
poised-soft <poised@cloakedpotion.com>
2025-03-08 20:02:39 +0000

    Feature: Encrypt the DB (#4)

--- docs/DEPENDENCIES.md
+++ docs/DEPENDENCIES.md
@@ -7,4 +7,6 @@ The table below contains the name, license, source, and a brief description of w
 | CouchDB | Apache 2.0 | Main Database | https://github.com/apache/couchdb
 | NodeJS | MIT | Used for local development | https://github.com/nodejs/node
 | NPM | [Artistic License 2.0](https://docs.npmjs.com/policies/npm-license) | Used for local development | https://github.com/npm
+| NVM | MIT | Node version managemetn | https://github.com/nvm-sh/nvm
+| OpenPGP.js | LGPL 3.0 | Encryption for budget DB | https://github.com/openpgpjs/openpgpjs
 | PouchDB | Apache 2.0 | Database for offline-first clients | https://github.com/pouchdb/pouchdb
--- src/budget/budget.js
+++ src/budget/budget.js
@@ -1,4 +1,6 @@
-async function initializeBudget() {
+import { getCurrentBudget, getCategories } from "../../node_modules/poised-budget-core/utils.js";
+
+async function initializeBudget(password) {
     const params = new URLSearchParams(document.location.search);
 
     if (!params.get("budgetMonth")) {
@@ -6,20 +8,14 @@ async function initializeBudget() {
         params.append("budgetMonth", `${date.toLocaleString("default", { month: "long" })}_${date.getFullYear()}`)
         window.location.replace(`/src/budget/budget.html?${params}`);
     }
-    
-    const currentBudget = await getCurrentBudget(params);
 
-    const categoriesDB = new PouchDB("categories");
-    let categoryResults = await categoriesDB.allDocs({ include_docs: true });
-    if (categoryResults.total_rows === 0) {
-        createNewCategoriesFromParams(params);
-        categoryResults = await categoriesDB.allDocs({ include_docs: true });
-    }
-    
-    const categories = categoryResults.rows.flatMap(c => c.doc).filter(c => c.category === undefined).sort((a, b) => a.order - b.order);
+    const currentBudget = await getCurrentBudget(PouchDB, openpgp, params, password);
+    const categoryResults = await getCategories(PouchDB, openpgp, params, password);
+
+    const categories = categoryResults.filter(c => c.category === undefined).sort((a, b) => a.order - b.order);
     const catDivs = [];
     for (let c = 0; c < categories.length; c++) {
-        const subCategories = categoryResults.rows.flatMap(c => c.doc).filter(s => s.category === categories[c]._id).sort((a, b) => a.order - b.order);
+        const subCategories = categoryResults.filter(s => s.category === categories[c]._id).sort((a, b) => a.order - b.order);
         const subCategoryDivs = [];
         for (let s = 0; s < subCategories.length; s++) {
             subCategoryDivs.push(`
@@ -47,67 +43,6 @@ async function initializeBudget() {
     document.getElementById("year").innerHTML = params.get("budgetMonth").split('_')[1];
 }
 
-async function saveBudget(event) {
-    event.preventDefault();
-    const params = new URLSearchParams(document.location.search);
-    const values = [...document.getElementsByTagName("input")].map(el => ({ id: el.id, value: parseFloat(el.value) })).filter(cat => cat.value !== 0);
-    console.log(values)
-    
-    const budgetDB = new PouchDB("budgets");
-    const budget = {
-        "_id": params.get("budgetMonth"),
-        "month": params.get("budgetMonth").slice('_')[0],
-        "budget": values
-    }
-    budgetDB.put(budget);
-}
-
-async function createNewCategoriesFromParams(params) {
-    const rawJSON = params.get("json");
-    const json = JSON.parse(rawJSON);
-    if (json.poisedBudgetVersion !== "0.0.1") {
-        throw new Error("Poised Budget version does not match existing versions.")
-    }
-    
-    const categoriesDB = new PouchDB("categories");
-    const categories = json.categories;
-    const docs = [];
-    for (let c = 0; c < categories.length; c++) {
-        const category = categories[c];
-        const categroyID = generateID(category.name);
-        const doc = {
-            "_id": categroyID,
-            "name": category.name,
-            "order": category.order
-        };
-
-        docs.push(doc);
-        
-        for (let s = 0; s < category.sub_categories.length; s++) {
-            const subCategory = category.sub_categories[s];
-            const doc = {
-                "_id": generateID(subCategory.name),
-                "name": subCategory.name,
-                "order": subCategory.order,
-                "category": categroyID
-            }
-
-            docs.push(doc);
-        }
-    }
-
-    const createResult = await categoriesDB.bulkDocs(docs);
-    // An array indicating responses either [true], [true, undefined], or [undefined]
-    const resultSet = [...new Set(createResult.flatMap((res) => res.ok))];
-    if (resultSet.includes(undefined)) {
-        throw new Error("Failed to save categories to DB.");
-    }
-}
-
-function generateID(name) {
-    return `${name.replaceAll(/\W/g, "_").toLowerCase()}${Math.floor(Math.random() * 1000)}`
-}
-
 function nextOrPreviousMonth(event, next = true) {
     event.preventDefault();
     const increment = next ? 1 : -1;
@@ -120,17 +55,4 @@ function nextOrPreviousMonth(event, next = true) {
     window.location.replace(`/src/budget/budget.html?${params}`);
 }
 
-async function getCurrentBudget(params) {
-    const budgetDB = new PouchDB("budgets");
-
-    try {
-        return (await budgetDB.get(params.get("budgetMonth")))?.budget
-    } catch (e) {
-        if (e.message === "missing") {
-            console.log("Could not find document. Loading empty budget...")
-            return []
-        }
-
-        throw e
-    }
-}
+export { initializeBudget, nextOrPreviousMonth }
